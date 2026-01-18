222b586f344e7c29cebaed868c2fa79608d884f9
Junior <eecfredes@gmail.com>
2026-01-17 21:42:51 -0300

    :package:Importing dependence

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+struct LinusTorvalds;
+
+impl LinusTorvalds {
+    fn said(&self) -> &str {
+        "Most good programmers do programming not because they expect to get paid /
+     or get adulation by the public, but because it is fun to program."
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let linus_torvalds = LinusTorvalds;
+    println!("Hello, -- {}!", linus_torvalds.said());
 }
