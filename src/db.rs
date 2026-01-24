63ba68f230dad28dbdba2830784eabeaf29561d9
Junior <eecfredes@gmail.com>
2026-01-24 20:21:01 -0300

    :rocket:DB connection done

--- src/db.rs
+++ src/db.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use async_trait::async_trait;
 use chrono::{DateTime, Utc};
 use sqlx::{Pool, Postgres};
@@ -13,5 +15,250 @@ pub struct DBClient {
 impl DBClient {
     pub fn new(pool: Pool<Postgres>) -> Self {
         DBClient { pool }
+    }
+}
+
+#[async_trait]
+pub trait UserExt {
+    async fn get_user(
+        &self,
+        user_id: Option<Uuid>,
+        name: Option<&str>,
+        email: Option<&str>,
+        token: Option<&str>,
+    ) -> Result<Option<User>, sqlx::Error>;
+
+    async fn get_users(&self, page: u32, limit: usize) -> Result<Vec<User>, sqlx::Error>;
+
+    async fn save_user<T: Into<String> + Send>(
+        &self,
+        name: T,
+        email: T,
+        password: T,
+        verification_token: T,
+        token_expires_at: DateTime<Utc>,
+    ) -> Result<User, sqlx::Error>;
+
+    async fn get_user_count(&self) -> Result<i64, sqlx::Error>;
+
+    async fn update_user_name<T: Into<String> + Send>(
+        &self,
+        user_id: Uuid,
+        name: T,
+    ) -> Result<User, sqlx::Error>;
+
+    async fn update_user_role(&self, user_id: Uuid, role: UserRole) -> Result<User, sqlx::Error>;
+
+    async fn update_user_password(
+        &self,
+        user_id: Uuid,
+        password: String,
+    ) -> Result<User, sqlx::Error>;
+
+    async fn verifed_token(&self, token: &str) -> Result<(), sqlx::Error>;
+
+    async fn add_verifed_token(
+        &self,
+        user_id: Uuid,
+        token: &str,
+        expires_at: DateTime<Utc>,
+    ) -> Result<(), sqlx::Error>;
+}
+
+#[async_trait]
+impl UserExt for DBClient {
+    async fn get_user(
+        &self,
+        user_id: Option<Uuid>,
+        name: Option<&str>,
+        email: Option<&str>,
+        token: Option<&str>,
+    ) -> Result<Option<User>, sqlx::Error> {
+        let mut user: Option<User> = None;
+
+        if let Some(user_id) = user_id {
+            user = sqlx::query_as!(
+                User,
+                r#"SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" FROM users WHERE id = $1"#,
+                user_id
+            ).fetch_optional(&self.pool).await?;
+        } else if let Some(name) = name {
+            user = sqlx::query_as!(
+                User,
+                r#"SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" FROM users WHERE name = $1"#,
+                name
+            ).fetch_optional(&self.pool).await?;
+        } else if let Some(email) = email {
+            user = sqlx::query_as!(
+                User,
+                r#"SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" FROM users WHERE email = $1"#,
+                email
+            ).fetch_optional(&self.pool).await?;
+        } else if let Some(token) = token {
+            user = sqlx::query_as!(
+                User,
+                r#"
+                SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" 
+                FROM users 
+                WHERE verification_token = $1"#,
+                token
+            )
+            .fetch_optional(&self.pool)
+            .await?;
+        }
+
+        Ok(user)
+    }
+
+    async fn get_users(&self, page: u32, limit: usize) -> Result<Vec<User>, sqlx::Error> {
+        let offset = (page.saturating_sub(1) as i64) * (limit as i64);
+
+        let users = sqlx::query_as!(
+            User,
+            r#"SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" FROM users 
+            ORDER BY created_at DESC LIMIT $1 OFFSET $2"#,
+            limit as i64,
+            offset,
+        ).fetch_all(&self.pool)
+        .await?;
+
+        Ok(users)
+    }
+
+    async fn save_user<T: Into<String> + Send>(
+        &self,
+        name: T,
+        email: T,
+        password: T,
+        verification_token: T,
+        token_expires_at: DateTime<Utc>,
+    ) -> Result<User, sqlx::Error> {
+        let user = sqlx::query_as!(
+            User,
+            r#"
+            INSERT INTO users (name, email, password,verification_token, token_expires_at) 
+            VALUES ($1, $2, $3, $4, $5) 
+            RETURNING id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole"
+            "#,
+            name.into(),
+            email.into(),
+            password.into(),
+            verification_token.into(),
+            token_expires_at
+        ).fetch_one(&self.pool)
+        .await?;
+        Ok(user)
+    }
+
+    async fn get_user_count(&self) -> Result<i64, sqlx::Error> {
+        let count: Option<i64> = sqlx::query_scalar!(r#"SELECT COUNT(*) FROM users"#)
+            .fetch_one(&self.pool)
+            .await?;
+
+        Ok(count.unwrap_or(0))
+    }
+
+    async fn update_user_name<T: Into<String> + Send>(
+        &self,
+        user_id: Uuid,
+        name: T,
+    ) -> Result<User, sqlx::Error> {
+        let user = sqlx::query_as!(
+            User,
+            r#"
+            UPDATE users
+            SET name = $1, updated_at = Now()
+            WHERE id = $2
+            RETURNING id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole"
+            "#,
+            name.into(),
+            user_id
+        ).fetch_one(&self.pool)
+        .await?;
+
+        Ok(user)
+    }
+
+    async fn update_user_role(
+        &self,
+        user_id: Uuid,
+        new_role: UserRole,
+    ) -> Result<User, sqlx::Error> {
+        let role_str: &str = new_role.to_str();
+        let user = sqlx::query_as::<_, User>(
+            r#"
+            UPDATE users
+            SET role = $1::user_role, updated_at = Now()
+            WHERE id = $2
+            RETURNING id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole"
+            "#,
+        )
+        .bind(role_str)
+        .bind(user_id)
+        .fetch_one(&self.pool)
+        .await?;
+
+        Ok(user)
+    }
+
+    async fn update_user_password(
+        &self,
+        user_id: Uuid,
+        new_password: String,
+    ) -> Result<User, sqlx::Error> {
+        let user = sqlx::query_as!(
+            User,
+            r#"
+            UPDATE users
+            SET password = $1, updated_at = Now()
+            WHERE id = $2
+            RETURNING id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole"
+            "#,
+            new_password,
+            user_id
+        ).fetch_one(&self.pool)
+        .await?;
+
+        Ok(user)
+    }
+
+    async fn verifed_token(&self, token: &str) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            r#"
+            UPDATE users
+            SET verified = true, 
+                updated_at = Now(),
+                verification_token = NULL,
+                token_expires_at = NULL
+            WHERE verification_token = $1
+            "#,
+            token
+        )
+        .execute(&self.pool)
+        .await?;
+
+        Ok(())
+    }
+
+    async fn add_verifed_token(
+        &self,
+        user_id: Uuid,
+        token: &str,
+        token_expires_at: DateTime<Utc>,
+    ) -> Result<(), sqlx::Error> {
+        let _ = sqlx::query!(
+            r#"
+            UPDATE users
+            SET verification_token = $1, token_expires_at = $2, updated_at = Now()
+            WHERE id = $3
+            "#,
+            token,
+            token_expires_at,
+            user_id,
+        )
+        .execute(&self.pool)
+        .await?;
+
+        Ok(())
     }
 }
